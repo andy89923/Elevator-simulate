5231a0e1ba7ea4cf96f5672e8bfccfb06db9276a
CTFang@WireLab <ctfang.cs12@nycu.edu.tw>
2024-10-19 08:08:54 +0000

    feat: add elevator class structure

--- elevator.cpp
+++ elevator.cpp
@@ -1,6 +1,9 @@
 #include <unistd.h>
 
+#include <chrono>
 #include <iostream>
+#include <mutex>
+#include <thread>
 
 #include "utils/socket.cpp"
 
@@ -15,6 +18,56 @@ void printElevatorMsg() {
 #define BUFFERSIZ 10
 char buf[BUFFERSIZ];
 
+enum Stauts { Close, Open, Move };
+
+struct State {
+    Stauts status;
+    int floor;
+
+    State(Stauts status, int floor) {
+        this->status = status;
+        this->floor = floor;
+    }
+
+    bool operator==(const State& rhs) {
+        return (status == rhs.status && floor == rhs.floor);
+    }
+};
+
+class Elevator {
+   protected:
+    State state;
+    mutex state_mutex;
+
+    void print_state() {
+        while (true) {
+            this_thread::sleep_for(std::chrono::seconds(1));
+            string state_str;
+            {
+                lock_guard<mutex> lock(state_mutex);
+                switch (state.status) {
+                    case Close:
+                        state_str = "Close";
+                        break;
+                    case Open:
+                        state_str = "Open";
+                        break;
+                    case Move:
+                        state_str = "Move";
+                        break;
+                }
+            }
+            cout << "Elevator status: " << state_str
+                 << ", Current floor: " << state.floor << "\n";
+        }
+    }
+
+   public:
+    Elevator() : state(Close, 1) {}
+
+    void run() { thread(&Elevator::print_state, this).detach(); }
+};
+
 void receiveCommand(int& controller_sock) {
     for (;;) {
         int read_len = read(controller_sock, buf, BUFFERSIZ);
@@ -22,6 +75,7 @@ void receiveCommand(int& controller_sock) {
             return;
         }
 
+        // TODO
         cout << read_len << " : " << buf << '\n';
     }
 }
@@ -44,12 +98,15 @@ int main(int argc, char* argv[]) {
     cout << "Openning server on " << server_ip << ":" << server_port << "\n";
     tcp_server(socket, server_ip, server_port);
 
+    Elevator elevator;
     printElevatorMsg();
+
+    elevator.run();
 
     for (;;) {
         int client_fd = acceptClient(socket);
 
-        receiveCommand(client_fd);
+        thread(receiveCommand, client_fd);
     }
     return 0;
 }
